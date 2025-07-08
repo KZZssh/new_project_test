1d794610176bdab27cb81b0bb09c433388879b09
KZZssh <alibekkudaibergen3@gmail.com>
2025-07-08 23:19:14 +0600

    I

--- configs.py
+++ configs.py
@@ -3,7 +3,17 @@ import os
 
 FLASK_UPLOAD_URL = os.getenv("FLASK_UPLOAD_URL", "https://flask-media-server.fly.dev/upload")
 
-BOT_TOKEN = "7014521370:AAHgMni3jXKU4n0hz7l-hFXigTTvseK8yiE"
+
+
+# Берем токен из "секретов" Fly.io (переменных окружения)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Проверка, что токен существует. Если нет, приложение упадет с понятной ошибкой.
+if not BOT_TOKEN:
+    raise ValueError("Секрет BOT_TOKEN не найден! Установите его командой: fly secrets set BOT_TOKEN=...")
+
+
+
 
 
 
